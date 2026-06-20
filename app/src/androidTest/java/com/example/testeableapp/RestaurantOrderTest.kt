29489785cf62ac5f9c6898caca222c1ef47cd451
Raquel-0105 <164411359+Raquel-0105@users.noreply.github.com>
2026-06-19 18:47:33 -0600

    Add files via upload

---
+++ app/src/androidTest/java/com/example/testeableapp/RestaurantOrderTest.kt
@@ -0,0 +1,15 @@
+package com.example.testeableapp
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import com.example.testeableapp.model.MenuData
+import org.junit.Rule
+import org.junit.Test
+
+class RestaurantOrderTest {
+
+}
+
---
+++ app/src/test/java/com/example/testeableapp/ExampleUnitTest.kt
@@ -0,0 +1,8 @@
+package com.example.testeableapp
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+
+}
